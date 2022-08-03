ecdf7704b183ca963db5bfc61bcb3a734d360ffb
Jordi Isidro Llobet <mail@jordiisidrollobet.tech>
2022-08-03 15:40:54 +0200

    taules normalitzades

---
+++ daily/models/month_energy_contract.sql
@@ -0,0 +1,10 @@
+{{ config(materialized='table') }}
+
+select
+date_trunc('month', ts) as ts, c.id as contract_id
+, sum(input_active_energy_kwh) as input_active_energy_kwh
+, sum(output_active_energy_kwh) as output_active_energy_kwh
+, current_timestamp as created_at, current_timestamp as updated_at
+from ods_curveregistry oc
+join contract c on c.comer_contractid =oc.contract
+group by c.id,  date_trunc('month', ts)
