70817088a3526e73b808565db471a1123aedae2b
Jordi Isidro Llobet <mail@jordiisidrollobet.tech>
2022-08-04 07:26:14 +0200

    energia per day, week, year

---
+++ daily/models/week_energy_community.sql
@@ -0,0 +1,10 @@
+{{ config(materialized='table') }}
+
+select  md5(concat(date_trunc('week', ts) ,'-', c.community_id))::UUID as id,
+date_trunc('week', ts) as ts, c.community_id as community_id
+, sum(input_active_energy_kwh) as input_active_energy_kwh
+, sum(output_active_energy_kwh) as output_active_energy_kwh
+, current_timestamp as created_at, current_timestamp as updated_at
+from ods_curveregistry oc
+join contract c on c.comer_contractid =oc.contract
+group by c.community_id,  date_trunc('week', ts)
