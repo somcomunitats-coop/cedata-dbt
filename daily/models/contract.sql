5a67903ed9a9dce91cd78a5cb30356e7e24befd3
Jordi Isidro Llobet <mail@jordiisidrollobet.tech>
2022-08-18 08:09:28 +0200

    correcció contract community duplicats

--- daily/models/contract.sql
+++ daily/models/contract.sql
@@ -1,9 +1,9 @@
 {{ config(materialized='incremental', unique_key='comer_contractid') }}
 
-select  coalesce(ct.id,rank() over( order by contract)+(select max(id)from contract)) as id
+select  coalesce(ct.id,row_number() over( order by ct.id, contract)+(select max(id)from contract)) as id
 , o.contract as comer_contractid, o.contract as description
     , coalesce(ct.created_at, current_date) as created_at, coalesce(ct.updated_at, current_Date) as updated_at
     ,case when o.date_end='9999-12-31' then true else false end as is_active, c.id as community_id
 from ods_contract_community o
-    join community c on o.community =c.name
+    join {{ref('community')}} c on o.community =c.name
     left join contract ct on o.contract =ct.comer_contractid and c.id=ct.id
