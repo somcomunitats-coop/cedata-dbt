0cdc3b5d1dd7ea59f877f7e846d752127863255b
Jordi Isidro Llobet <mail@jordiisidrollobet.tech>
2022-08-18 08:34:32 +0200

    correcció incrementals year contracte actiu

--- daily/models/year_energy_community.sql
+++ daily/models/year_energy_community.sql
@@ -7,7 +7,7 @@ date_trunc('year', ts) as ts, c.community_id as community_id
 , sum(output_active_energy_kwh) as output_active_energy_kwh
 , current_timestamp as created_at, current_timestamp as updated_at
 from ods_curveregistry oc
-join {{ ref('contract') }} c on c.comer_contractid =oc.contract
+join {{ ref('contract') }} c on c.comer_contractid =oc.contract and c.is_active=true
 where date_trunc('year', ts)>=(select date_trunc('year', min(ts))
                                from ods_curveregistry oc
                                where oc.updated_at>=current_date-5)
