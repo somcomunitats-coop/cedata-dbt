797c0375301d4fe737866ba5d8d0630d3f12956f
Jordi Isidro Llobet <mail@jordiisidrollobet.tech>
2024-04-22 09:12:18 +0200

    inm taules odoo

---
+++ daily/models/inm_projectes_autoconsum_comunitat.sql
@@ -0,0 +1,20 @@
+{{ config(materialized='incremental'
+ , unique_key='data'
+ , post_hook=after_commit('create index IF NOT EXISTS cix_inm_projectes_autoconsum_comunitat on {{ this }} (data, id_community); CLUSTER {{ this }} USING cix_inm_projectes_autoconsum_comunitat;')
+ , schema='inm'
+ , docs={'node_color': '#b3b325'}
+) }}
+
+
+select eprj.company_id as id_community, sum(esc.power) as pw_autoconsum, count(*) as cnt_autoconsum
+from  {{ source('dwhexternal', 'hist_odoo_energy_selfconsumption_selfconsumption')}} as esc
+    join {{ source('dwhpublic', 'data')}} d on d.data>=esc.dt_start and d.data<esc.dt_end
+    join {{ source('dwhexternal', 'hist_odoo_energy_project_project')}} as eprj on esc.project_id = eprj.id and esc.data=eprj.data
+where eprj.state <> 'draft'
+    and d.data<=current_date
+
+    {% if is_incremental() %}
+    and d.data>=current_date-5
+    {% endif %}
+group by d.data, company_id
+
---
+++ daily/models/inm_socies_comunitat.sql
@@ -0,0 +1,21 @@
+{{ config(materialized='incremental'
+ , unique_key='data'
+ , post_hook=after_commit('create index IF NOT EXISTS cix_inm_socies_comunitat on {{ this }} (data, id_community); CLUSTER {{ this }} USING cix_inm_socies_comunitat;')
+ , schema='inm'
+ , docs={'node_color': '#b3b325'}
+) }}
+
+select d.data, rel.res_company_id as id_community, count(*) as socies
+	from  {{ source('dwhexternal', 'hist_odoo_res_company_res_partner_rel')}} rel
+	join {{ source('dwhpublic', 'data')}} d on d.data>=rel.dt_start and d.data<rel.dt_end
+	join {{ source('dwhexternal', 'hist_odoo_res_partner')}} rp on rp.id = rel.res_partner_id and rel.data=rp.data
+
+where rp.cooperator_register_number is not null
+    and d.data<=current_date
+
+    {% if is_incremental() %}
+    and d.data>=current_date-5
+    {% endif %}
+
+group by d.data, rel.res_company_id
+
