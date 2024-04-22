423127902b8836944a7d52ce2fa38f7bc25e0b3d
Jordi Isidro Llobet <mail@jordiisidrollobet.tech>
2024-04-22 09:19:02 +0200

    inm taules odoo

--- daily/models/inm_projectes_autoconsum_comunitat.sql
+++ daily/models/inm_projectes_autoconsum_comunitat.sql
@@ -6,7 +6,7 @@
 ) }}
 
 
-select eprj.company_id as id_community, sum(esc.power) as pw_autoconsum, count(*) as cnt_autoconsum
+select d.data, eprj.company_id as id_community, sum(esc.power) as pw_autoconsum, count(*) as cnt_autoconsum
 from  {{ source('dwhexternal', 'hist_odoo_energy_selfconsumption_selfconsumption')}} as esc
     join {{ source('dwhpublic', 'data')}} d on d.data>=esc.dt_start and d.data<esc.dt_end
     join {{ source('dwhexternal', 'hist_odoo_energy_project_project')}} as eprj on esc.project_id = eprj.id
