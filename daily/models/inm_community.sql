35dee06576143102bf89ab33d222a23548465f17
Jordi Isidro Llobet <mail@jordiisidrollobet.tech>
2024-04-24 11:40:01 +0200

    percentatge activació

--- daily/models/inm_community.sql
+++ daily/models/inm_community.sql
@@ -11,11 +11,12 @@ select orc.id as id_community, orc.legal_form as community_legal_form, orc.name 
 	, orc.allow_new_members, orc.create_date as community_create_date
 	, orc.parent_id as id_coordinator, orc.partner_id as community_id_partner
 	, lp.community_type, coalesce(lp.community_status,'') as community_status
-	, 100::numeric as completed_percentage
+	, coalesce(h.completed_percentage, 100::numeric) as completed_percentage
 	, d.data
 from {{ source('dwhexternal', 'hist_odoo_res_company')}} orc
     join {{ source('dwhpublic', 'data')}} d on d.data>=orc.dt_start and d.data<orc.dt_end
     left join {{ source('dwhexternal', 'hist_odoo_landing_page')}} lp on orc.landing_page_id=lp.id and d.data>=lp.dt_start and d.data<lp.dt_end
+    left join {{ source('dwhexternal', 'hist_odoo_cm_place')}} h on lp.map_place_id=h.id
 where hierarchy_level ='community'
     and orc.name not ilike '%DELETE%'
     and orc.name not ilike '%Prova%'
