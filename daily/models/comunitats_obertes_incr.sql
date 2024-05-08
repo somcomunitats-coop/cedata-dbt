8a9e53cc59230750c9b9e091f91beb8bcea47e2d
Jordi Isidro Llobet <mail@jordiisidrollobet.tech>
2024-05-08 08:12:59 +0200

    comunitats_obertes_incr

---
+++ daily/models/comunitats_obertes_incr.sql
@@ -0,0 +1,19 @@
+{{ config(materialized='incremental'
+ , unique_key='data'
+ , post_hook=after_commit('create index IF NOT EXISTS cix_comunitats_obertes_incr on {{ this }} (data); CLUSTER {{ this }} USING cix_comunitats_obertes_incr;')
+) }}
+
+
+select co.*
+	, case when ca.id_community is null then 1 when co.id_community is null then -1 else 0 end as increment_comunitat
+	, coalesce(co.coordinator_sr_amount_untaxed,0)-coalesce(ca.coordinator_sr_amount_untaxed,0) as increment_coordinator_sr_amount_untaxed
+	, coalesce(co.community_completed_percentage,0)-coalesce(ca.community_completed_percentage,0) as increment_community_completed_percentage
+	, coalesce(co.community_sr_amount_untaxed,0)-coalesce(ca.community_sr_amount_untaxed,0) as increment_community_sr_amount_untaxed
+	, coalesce(co.socies,0)-coalesce(ca.socies,0) as increment_socies
+from {{ref('comunitats_obertes')}} co
+	full join {{ref('comunitats_obertes')}} ca on co.data=ca.data+1 and co.id_community=ca.id_community
+where co.data<=CURRENT_DATE
+    {% if is_incremental() %}
+        and co.data>=current_date-5
+    {% endif %}
+
