b7b47aeecd3adf58a9dbdc45e8e7e81f12aee15d
Jordi Isidro Llobet <mail@jordiisidrollobet.tech>
2024-05-14 12:43:41 +0200

    capital social voluntari vs obligatori

--- daily/models/inm_company_subscription_request.sql
+++ daily/models/inm_company_subscription_request.sql
@@ -7,8 +7,8 @@
 
 
 select d.data, hsr.company_id, sum(hm.amount_untaxed) as sr_amount_untaxed
-    , sum(case when hst.is_voluntary then hm.amount_untaxed end) as sr_amount_untaxed_voluntary
-    , sum(case when not hst.is_voluntary then hm.amount_untaxed end) as sr_amount_untaxed_mandatory
+    , sum(case when hsr.is_voluntary then hm.amount_untaxed end) as sr_amount_untaxed_voluntary
+    , sum(case when not hsr.is_voluntary then hm.amount_untaxed end) as sr_amount_untaxed_mandatory
 from {{ source('dwhexternal', 'hist_odoo_subscription_request')}} hsr
 join {{ source('dwhpublic', 'data')}} d on d.data>=hsr.dt_start and d.data<hsr.dt_end
 join {{ source('dwhexternal', 'hist_odoo_account_move')}} hm on  hm.subscription_request = hsr.id and d.data>=hm.dt_start and d.data<hm.dt_end
