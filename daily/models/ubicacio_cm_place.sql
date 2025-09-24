617c561aeed61b4a5ec85ec729d63fedfeca882c
Jordi Isidro Llobet <mail@jordiisidrollobet.tech>
2025-09-24 09:33:58 +0200

    ubicació dinàmica

--- daily/models/ubicacio_cm_place.sql
+++ daily/models/ubicacio_cm_place.sql
@@ -2,9 +2,14 @@
 {{ config(materialized='table'
 ) }}
 
-select u.id::int as id, u.municipio as municipi, g.comarca, g.provincia, g.ccaa, u.codpostal
-from  {{ ref('ubicacio_calc')}} u
-	left join (
+select u.id
+    , coalesce(g.municipi, u.municipi) as municipi
+    , g.comarca
+    , coalesce(g.provincia, u.provincia) as provincia
+    , coalesce(g.ccaa, u.ccaa) as ccaa
+    , u.codpostal
+from  {{ source('dwhexternal', 'geography_cm_place')}} u
+left join (
 		select cp
             , max(name_ccaa) as ccaa, max(name_provincia) as provincia, max(name_comarca) as comarca
             , max(name_municipi) as municipi
