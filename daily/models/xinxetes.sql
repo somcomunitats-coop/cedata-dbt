9a7f471c8f0fe888d9d8fbd351b65d7cba23022b
Jordi Isidro Llobet <mail@jordiisidrollobet.tech>
2025-10-02 16:32:19 +0200

    nou camp ccee_externas

--- daily/models/xinxetes.sql
+++ daily/models/xinxetes.sql
@@ -12,33 +12,33 @@ select d.data
     , ub.municipi, ub.comarca, ub.provincia, ub.ccaa, ub.codpostal
     , pc.name as name_place_category
 	,count(cmp.id)as xinxetes_sumat
-	,sum(case when (subm.submissions = 0 or subm.submissions is null) then 1 else 0 end) as xinxetes_sumat_sense_persones
-	,sum(case when subm.submissions > 0 then 1 else 0 end) as xinxetes_sumat_amb_persones
-	,sum(case when (cmp.completed_percentage < 50.0 and subm.submissions > 0) then 1 else 0 end ) as xinxetes_sumat_sota_50
-	,sum(case when (cmp.completed_percentage >= 50.0 and cmp.completed_percentage < 100.0 and subm.submissions > 0) then 1 else 0 end ) as xinxetes_sumat_50_100
-	,sum(subm.submissions) as persones
-	,sum(subm.leaders) as persones_leaders
-	,sum(subm.low_implication) as low_implication
-	,sum(subm.medium_implication) as medium_implication
-	,sum(subm.high_implication) as high_implication
-	,sum(subm.leadership_implication) as leadership_implication
-	,sum(subm.agregacio_i_flexibilitat_de_la_demanda) as agregacio_i_flexibilitat_de_la_demanda
-    ,sum(subm.formacio_ciutadana) as formacio_ciutadana
-    ,sum(subm.compres_collectives) as compres_collectives
-    ,sum(subm.generacio_renovable_comunitaria) as generacio_renovable_comunitaria
-    ,sum(subm.eficiencia_energetica) as eficiencia_energetica
-    ,sum(subm.subministrament_energia_100perc_renovable) as subministrament_energia_100perc_renovable
-    ,sum(subm.mobilitat_sostenible) as mobilitat_sostenible
-    ,sum(subm.energia_terminca_i_climatitzacio) as energia_terminca_i_climatitzacio
-    ,count(case when cmp.key_submissions_target_reached then 1 end) as grup_motor_potencial
-    ,count(case when cmp.key_group_activated then 1 end) as grup_motor_activat
-    ,sum(case when cmp.key_submissions_target_reached then subm.leaders else 0 end) as persones_leaders_cmp
-    ,sum(case when cmp.key_group_activated then subm.leaders else 0 end) as persones_leaders_cma
-    ,sum(case when cmp.key_submissions_target_reached then subm.submissions else 0 end) as persones_cmp
-    ,sum(case when cmp.key_group_activated then subm.submissions else 0 end) as persones_cma
-    ,sum(case when (cmp.completed_percentage >= 100.0 and subm.submissions > 0) then 1 else 0 end ) as xinxetes_sumat_100_o_mes
-    ,sum(case when subm.leaders>0 then 1 end ) as xinxetes_amb_leaders
-    ,sum(case when subm.leaders>0 then submissions end ) as persones_en_xinxetes_amb_leaders
+	,sum(case when (cmp.presenter_model_id = 4 and subm.submissions = 0 or subm.submissions is null) then 1 else 0 end) as xinxetes_sumat_sense_persones
+	,sum(case when cmp.presenter_model_id = 4 and subm.submissions > 0 then 1 else 0 end) as xinxetes_sumat_amb_persones
+	,sum(case when (cmp.presenter_model_id = 4 and cmp.completed_percentage < 50.0 and subm.submissions > 0) then 1 else 0 end ) as xinxetes_sumat_sota_50
+	,sum(case when (cmp.presenter_model_id = 4 and cmp.completed_percentage >= 50.0 and cmp.completed_percentage < 100.0 and subm.submissions > 0) then 1 else 0 end ) as xinxetes_sumat_50_100
+	,sum(case when cmp.presenter_model_id = 4 then subm.submissions end) as persones
+	,sum(case when cmp.presenter_model_id = 4 then subm.leaders end) as persones_leaders
+	,sum(case when cmp.presenter_model_id = 4 then subm.low_implication end) as low_implication
+	,sum(case when cmp.presenter_model_id = 4 then subm.medium_implication end) as medium_implication
+	,sum(case when cmp.presenter_model_id = 4 then subm.high_implication end) as high_implication
+	,sum(case when cmp.presenter_model_id = 4 then subm.leadership_implication end) as leadership_implication
+	,sum(case when cmp.presenter_model_id = 4 then subm.agregacio_i_flexibilitat_de_la_demanda end) as agregacio_i_flexibilitat_de_la_demanda
+    ,sum(case when cmp.presenter_model_id = 4 then subm.formacio_ciutadana end) as formacio_ciutadana
+    ,sum(case when cmp.presenter_model_id = 4 then subm.compres_collectives end) as compres_collectives
+    ,sum(case when cmp.presenter_model_id = 4 then subm.generacio_renovable_comunitaria end) as generacio_renovable_comunitaria
+    ,sum(case when cmp.presenter_model_id = 4 then subm.eficiencia_energetica end) as eficiencia_energetica
+    ,sum(case when cmp.presenter_model_id = 4 then subm.subministrament_energia_100perc_renovable end) as subministrament_energia_100perc_renovable
+    ,sum(case when cmp.presenter_model_id = 4 then subm.mobilitat_sostenible end) as mobilitat_sostenible
+    ,sum(case when cmp.presenter_model_id = 4 then subm.energia_terminca_i_climatitzacio end) as energia_terminca_i_climatitzacio
+    ,count(case when cmp.presenter_model_id = 4 and cmp.key_submissions_target_reached then 1 end) as grup_motor_potencial
+    ,count(case when cmp.presenter_model_id = 4 and cmp.key_group_activated then 1 end) as grup_motor_activat
+    ,sum(case when cmp.presenter_model_id = 4 and cmp.key_submissions_target_reached then subm.leaders else 0 end) as persones_leaders_cmp
+    ,sum(case when cmp.presenter_model_id = 4 and cmp.key_group_activated then subm.leaders else 0 end) as persones_leaders_cma
+    ,sum(case when cmp.presenter_model_id = 4 and cmp.key_submissions_target_reached then subm.submissions else 0 end) as persones_cmp
+    ,sum(case when cmp.presenter_model_id = 4 and cmp.key_group_activated then subm.submissions else 0 end) as persones_cma
+    ,sum(case when (cmp.presenter_model_id = 4 and cmp.completed_percentage >= 100.0 and subm.submissions > 0) then 1 else 0 end ) as xinxetes_sumat_100_o_mes
+    ,sum(case when cmp.presenter_model_id = 4 and subm.leaders>0 then 1 end ) as xinxetes_amb_leaders
+    ,sum(case when cmp.presenter_model_id = 4 and subm.leaders>0 then submissions end ) as persones_en_xinxetes_amb_leaders
     ,count(case when cmp.presenter_model_id=7 then 1 end) as ccee_externas
     , m.name as mapa, rp.name as company
 from {{ source('dwhpublic', 'data')}} d
@@ -69,7 +69,7 @@ left join (
 	group by place_id, data
 	) subm on subm.place_id = cmp.id and subm.data=d.data
 where 1=1
-    --and cmp.presenter_model_id = 4  -- per activar 20251002
+    --and cmp.presenter_model_id = 4  -- per activar 20251002, passat a les mètriques
     --and cmp.company_id = 1 -- instancia
     and cmp.status = 'published'
     and cmp.active
