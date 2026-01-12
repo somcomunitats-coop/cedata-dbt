644fc2bd7fdecdff86c16387ecd0c010d8faa002
Jordi Isidro Llobet <mail@jordiisidrollobet.tech>
2026-01-12 12:39:03 +0100

    afegir les coordinadores també com a comunitat

--- daily/models/inm_community.sql
+++ daily/models/inm_community.sql
@@ -59,3 +59,23 @@ where not exists
     {% if is_incremental() %}
     and d.data>=current_date-5
     {% endif %}
+
+-- afegim també les coordinadores com a comunitat
+select orc.id as id_coordinator, orc.legal_form as coordinator_legal_form, 'Coord. '||orc.name as coordinator_name
+, orc.comercial_name  as coordinator_comercial_name, orc.email as coordinator_email
+, orc.create_date as coordinator_create_date
+, orc.parent_id as id_instance, orc.partner_id as coordinator_id_partner
+, lp.community_type, null as community_status
+, d.data
+from {{ source('dwhexternal', 'hist_odoo_res_company')}} orc
+    join {{ source('dwhpublic', 'data')}} d on d.data>=orc.dt_start and d.data<dt_end
+    left join {{ source('dwhexternal', 'hist_odoo_landing_page')}} lp on orc.landing_page_id=lp.id and d.data>=lp.dt_start and d.data<lp.dt_end
+where hierarchy_level ='coordinator'
+    and orc.name not ilike '%DELETE%'
+    and orc.name not ilike '%ON-HOLD%'
+    and orc.name not ilike '%Prova%'
+    and d.data<=current_date
+
+    {% if is_incremental() %}
+    and d.data>=current_date-5
+    {% endif %}
